653dc6f7400538b8681c1c5a035d4561f6cf3311
zazadupond <pocketbookjava@gmail.com>
2019-11-19 00:42:03 +0300

    Task4

--- src/by/epam/stady/stringcharacterarray/Task3.java
+++ src/by/epam/stady/stringcharacterarray/Task3.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Task3 {
     public static void main(String[] args) {
-        String line = " 965 г. - Разгром Хазарского каганата войском Киевского князя Святослава Игоревича. " +
+        String line = "  965 5 6 67г. - Разгром Хазарского каганата войском Киевского князя Святослава Игоревича. ";/* +
                 "988 г. - Крещение Руси. Киевская Русь принимает православное христианство. " +
                 "1223 г. - Битва на Калке - первое сражение между русскими и моголами." +
                 "1240 г. - Невская битва - военный конфликт между русскими_test12345_test во главе с новгородским князем Александром и шведами." +
@@ -17,22 +17,18 @@ public class Task3 {
                         "1556 г. — Присоединение Астраханского ханства к Москвкой Руси.\n" +
                 "\n" +
                 "1558 — 1583 г. — Ливонская война. Война Русского царства против Ливонского ордена и последующий конфликт Русского царства с Великим княжеством Литовским, Польшей и Швецией.";
+*/
 
-
-        searchCountNumbersInLine(line);
+        searchCountDigitInLine(line);
     }
 
 
-    public static int searchCountNumbersInLine(String line){
+    public static int searchCountDigitInLine(String line){
 
-        String[] lineArray = line.replaceAll("[^0-9]+", " ").trim().split(" ");
+        int count = line.replaceAll("[^0-9]+", "").length(); /* ^ - убираем все строки (без ^ - останутся наоборот только слова без чисел), заменяем убранные строки на
+        без пробела, получается строка с набором цифр без пробелов. Узнаем длину строки*/
+        System.out.println(line.replaceAll("[^0-9]+", "").length());
 
-        for (int i = 0; i < lineArray.length; i++){
-            System.out.println(i + " " + lineArray[i] + " ");
-
-        }
-        System.out.println("\n" + lineArray.length);
-
-        return lineArray.length;
+        return count;
     }
 }
