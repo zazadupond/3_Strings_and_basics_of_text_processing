8d753cd647860e2253f414c9bf9e935975b95697
zazadupond <pocketbookjava@gmail.com>
2019-11-27 00:35:26 +0300

    Task1

--- src/by/epam/stady/stringcharacterarray/Task1.java
+++ src/by/epam/stady/stringcharacterarray/Task1.java
@@ -13,13 +13,36 @@ public class Task1 {
             System.out.println(arrayString[i]);
         }
     }
+
     public static String[] camelCaseInSnakeCase( String array[]){
+
+        String str = "";
+
+        for (int i = 0; i < array.length; i++){
+            char[] chArray = array[i].toCharArray();
+            for (int j = 0; j < chArray.length; j++){
+                if (Character.isUpperCase(chArray[j])){
+                    str = str.concat("_").concat(String.valueOf(chArray[j]));
+
+                } else {
+                    str = str.concat(String.valueOf(chArray[j]));
+                }
+            }
+
+            array[i] = str.toLowerCase();
+            str = "";
+        }
+
+        return array;
+    }
+
+    /*public static String[] camelCaseInSnakeCase( String array[]){
 
         for (int i = 0; i < array.length; i++) {
             array[i] = array[i].replaceAll("([A-Z]+)","\\_$1").toLowerCase(); //"\\_$1"  вставляем знак _ перед каждой большой буквой.
         }
 
         return array;
-    }
+    }*/
 
 }
