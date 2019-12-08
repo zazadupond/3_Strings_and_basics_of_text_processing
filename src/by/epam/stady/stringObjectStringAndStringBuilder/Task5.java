377c25f82369a2bd8c8b2927a5fd4f94742aa5c5
zazadupond <pocketbookjava@gmail.com>
2019-12-09 00:23:37 +0300

    Task4

---
+++ src/by/epam/stady/stringObjectStringAndStringBuilder/Task5.java
@@ -0,0 +1,22 @@
+package by.epam.stady.stringObjectStringAndStringBuilder;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+        String txt = " abbabbbaaaaabbbabababab";
+        countCharacter(txt);
+        System.out.println(countCharacter(txt));
+    }
+
+    static int countCharacter(String text){
+
+        int countCh = 0;
+        for (int i = 0; i < text.length(); i++){
+            if (text.charAt(i) == 'a'){
+                countCh++;
+            }
+        }
+        return countCh;
+    }
+
+}
